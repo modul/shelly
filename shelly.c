d2492d99571c8256d187012f70e992399c319b32
Remo Giermann <mo@liberejo.de>
2012-08-17 03:11:03 +0200

    some tweaks, error codes, no printing

--- shelly.c
+++ shelly.c
@@ -17,125 +17,82 @@ void forget()
 	memset(tape, 0, SHELLY_TAPESIZE);
 }
 
-int shelly(const char *input)
+int shelly(const char *ip)
 {
 	unsigned short tmp;
 	unsigned short loop = 0;
 
-	while (*input) {
-		switch (*input) 
-		{
-			case '+':
-				tape[tp] += 1;
+	while (*ip) {
+		if (*ip == '+') {
+			tape[tp] += 1;
 #ifndef SHELLY_WRAPPING_CELL
-				if (tape[tp] == 0)
-					tape[tp] -= 1;
+			if (tape[tp] == 0)
+				return SHELLY_OVERFLW;
 #endif
-			break;
-
-			case '-':
-				tape[tp] -= 1;
+		}
+		else if (*ip == '-') {
+			tape[tp] -= 1;
 #ifndef SHELLY_WRAPPING_CELL
-				if (tape[tp] == 255)
-					tape[tp] += 1;
+			if (tape[tp] == 255)
+				return SHELLY_UNDRFLW;
 #endif
-			break;
-
-			case '>':
-				if (tp < SHELLY_TAPESIZE-1)
-					tp++;
-#ifdef SHELLY_WRAPPING_TAPE
-				else
-					tp = 0;
+		}
+		else if (*ip == '>') {
+			if (tp < SHELLY_TAPESIZE-1)
+				tp++;
+			else
+#ifndef SHELLY_WRAPPING_TAPE
+				return SHELLY_TAPELIM;
+#else
+				tp = 0;
 #endif
-			break;
-
-			case '<':
-				if (tp > 0)
-					tp--;
-#ifdef SHELLY_WRAPPING_TAPE
-				else
-					tp = SHELLY_TAPESIZE-1;
+		}
+		else if (*ip == '<') {
+			if (tp > 0)
+				tp--;
+			else
+#ifndef SHELLY_WRAPPING_TAPE
+				return SHELLY_TAPELIM;
+#else
+				tp = SHELLY_TAPESIZE-1;
 #endif
-			break;
-
-			case '.':
-				putchar((char) tape[tp]);
-			break;
-
-			case ',':
-				tape[tp] = (uint8_t) getchar();
-				if (tape[tp] == SHELLY_EOF)
-					tape[tp] = SHELLY_REAL_EOF;
-			break;
-
-			case '[':
-				loop = 1;
-				if (tape[tp] == 0) {
-					while (loop) {
-						if (*(++input) == '[')
-							loop++;
-						else if (*input == ']')
-							loop--;
-						else if (*input == 0) {
-							puts_P(PSTR("a lonesome [ ..."));
-							return -1;
-						}
-					}
-				}
-			break;
-
-			case ']':
-				if (tape[tp]) {
-					loop = 1;
-					while (loop) {
-						if (*(--input) == '[') 
-							loop--;
-						else if (*input == ']')
-							loop++;
-						else if (*input == 0) {
-							puts_P(PSTR("a lonesome ] ..."));
-							return -1;
-						}
-					}
-				}
-			break;
+		}
+		else if (*ip == '.') 
+			putchar((char) tape[tp]);
+		else if (*ip == ',') {
+			tape[tp] = (uint8_t) getchar();
+			if (tape[tp] == SHELLY_EOF) tape[tp] = SHELLY_REAL_EOF;
+		}
+		else if (*ip == '[' && tape[tp] == 0) {
+			loop = 1;
+			while (loop) {
+				if (*(++ip) == ']') loop--;
+				else if (*ip == '[') loop++;
+				else if (*ip == 0) return SHELLY_UNMATCH;
+			}
+		}
+		else if (*ip == ']' && tape[tp]) {
+			loop = 1;
+			while (loop) {
+				if (*(++ip) == ']') loop++;
+				else if (*ip == '[') loop--;
+				else if (*ip == 0) return SHELLY_UNMATCH;
+			}
+		}
 
 			/* totally extended commands */
 
-			case '_': // delay 
-				for (tmp=0; tmp<tape[tp]; tmp++) {
-					_delay_ms(10);
-				}
-			break;
-
-			case '(': // binary left shift
-				tape[tp] <<= 1;
-			break;
-
-			case ')': // binary right shift
-				tape[tp] >>= 1;
-			break;
-
-			case '{': // decimal left shift
-				tape[tp] /= 10;
-			break;
-
-			case '}': // decimal right shift
-				tape[tp] *= 10;
-			break;
-
-			case '@': // random integer
-				srand(tape[tp]);
-				tape[tp] = (uint8_t) rand();
-			break;
+		else if (*ip == '_') 
+			for (tmp=0; tmp<tape[tp]; tmp++, _delay_ms(10));
+		else if (*ip == '(') tape[tp] <<= 1;
+		else if (*ip == ')') tape[tp] >>= 1;
+		else if (*ip == '{') tape[tp] /= 10;
+		else if (*ip == '}') tape[tp] *= 10;
+		else if (*ip == '@') {
+			srand(tape[tp]);
+			tape[tp] = (uint8_t) rand();
 		}
-		input++;
+		ip++;
 	}
-
-	if (loop > 0) {
-		puts_P(PSTR("unmatched loops"));
-		return -1;
-	}
-	return 0;
+	return SHELLY_SUCCESS;
 }
