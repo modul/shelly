b3424673c27b3cf953004ea16a687cf1d4cae54f
Remo Giermann <mo@liberejo.de>
2012-08-17 04:33:14 +0200

    printing returned error codes

--- main.c
+++ main.c
@@ -13,14 +13,22 @@
 extern uint8_t tape[SHELLY_TAPESIZE];
 extern unsigned short tp;
 
+//#define DEBUG
+
 int main()
 {
+	int e;
 	char line[128];
 	init_stdio();
+
 	while (42) {
 		gets(line);
-		shelly(line);
+		if ((e = shelly(line)) != SHELLY_SUCCESS) {
+			putchar(e); putchar(10);
+		}
+#ifdef DEBUG
 		printf_P(PSTR("@% 3u [% 3u]\n"), tp, tape[tp]);
+#endif
 	}
 	return 0;
 }
