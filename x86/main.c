e274e7c317e0dd8b86270d81c3f701b521277174
Remo Giermann <mo@liberejo.de>
2012-08-19 03:45:32 +0200

    re-arranged a bit

--- x86/main.c
+++ x86/main.c
@@ -3,64 +3,59 @@
 #include <stdio.h>
 
 #include "shelly.h"
-#define LINE 256
 
-void exec(FILE *f, char *b, size_t len)
+extern uint8_t tape[SHELLY_TAPESIZE];
+extern unsigned short tp;
+
+int exec(FILE *f, size_t len)
 {
+	char *b = (char *) malloc(len);
 	char c, *s = b;
+	int r;
+
 	memset(b, 0, len);
 	while ((c = getc(f)) > 0 && c != '!') {
 		*b++ = c;
-		if (!len--) {
-			puts("BUFFULL");
-			free(s);
-			exit(255);
-		}
+		if (!len--) { free(s); exit(255); }
 	}
-	shelly(s);
+	r = shelly(s);
+	free(s);
+	return r;
 }
 
 int main(int argc, const char *argv[])
 {
-	FILE *fp = stdin;
-	if (argc > 1) {
-		if (strcmp(argv[1], "-i") == 0) {
-			extern uint8_t tape[SHELLY_TAPESIZE];
-			extern unsigned short tp;
-			char line[LINE] = {0};
+	if (argc == 1)
+		return exec(stdin, 65535);
+	else {
+		if (strcmp(argv[1], "-h") == 0) {
+			printf("Usage: %s [-i|-h|FILE]\n", argv[0]);
+			return 0;
+		}
+		else if (strcmp(argv[1], "-i") == 0) {
 			int e = 0;
+			char line[BUFSIZ] = {0};
 
 			while (42) {
 				printf(" [%c % 3u % 3u] ", e==0?'_':e, tp, tape[tp]);
-				if (!fgets(line, LINE, stdin)) return 0;
-				if ((e = strlen(line)) > 1 && line[e-2] == '\\') fgets(line+e-2, LINE-e, stdin);
+				if (!fgets(line, BUFSIZ, stdin)) return 0;
+				if ((e = strlen(line)) > 1 && line[e-2] == '\\') fgets(line+e-2, BUFSIZ-e, stdin);
 				if (*line == 10) {e = 0; continue;}
 				e = shelly(line);
 			}
 		}
-		else if (strcmp(argv[1], "-h") == 0) {
-			printf("Usage: %s [-i|-h|FILE]\n", argv[0]);
-			return 0;
-		}
 		else {
+			FILE *fp;
+			size_t len;
 			if (!(fp = fopen(argv[1], "r"))) {
 				perror(argv[1]);
 				return 128;
 			}
-			else {
-				char *buf;
-				size_t len;
-				fseek(fp, 0, SEEK_END);
-				len = ftell(fp);
-				buf = (char *) malloc(len);
-				rewind(fp);
-				exec(fp, buf, len);
-			}
+			fseek(fp, 0, SEEK_END);
+			len = ftell(fp);
+			rewind(fp);
+			return exec(fp, len);
 		}
-	}
-	else {
-		char *buf = malloc(65535);
-		exec(stdin, buf, 1);
 	}
 	return 0;
 }
